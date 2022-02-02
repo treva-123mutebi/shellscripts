d6169367c5b7338fdf6a78585970ad5cf9578fff
Mutebi David <mdavid.trevor@gmail.com>
2022-02-02 19:10:52 +0300

    31084 | md5sum | head -c 20

--- git.sh
+++ git.sh
@@ -6,5 +6,5 @@ git init
 git add .
 git commit -m "${var:=$RANDOM | md5sum | head -c 20}"
 git branch -M main
-git remote add origin https://github.com/treva-123mutebi/shellscripts.git
+
 git push -u origin main
